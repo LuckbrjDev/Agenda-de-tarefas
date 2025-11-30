ee081b36843a3134e00518f57b5666810c403b3e
LuckbrjDev <luckbrj.dev@gmail.com>
2025-11-30 13:55:17 -0300

    create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjDmwbmCTAbwTCFohIQVL-nJWqi_bso24",
+  authDomain: "agenda-de-tarefas-d273f.firebaseapp.com",
+  projectId: "agenda-de-tarefas-d273f",
+  storageBucket: "agenda-de-tarefas-d273f.firebasestorage.app",
+  messagingSenderId: "530787250320",
+  appId: "1:530787250320:web:c91207e9c431ee51f9e815"
+};
+
